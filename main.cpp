37e1f7c5fe32400c7679ebc32c5f259ed14d93f3
kidakorn-workplace <kidakorn.lik@student.mahidol.edu>
2026-03-30 08:35:33 +0000

    hhhh

--- main.cpp
+++ main.cpp
@@ -9,7 +9,18 @@
 using namespace std;
 #include "monster.h"
 
+
+monster *p,x;
+p=
 int main(int argc, const char * argv[]) {
- 
+    monster m1("inw in shadow",10,1);
+    monster m2("kratos",20,1),m4("lll",30);
+    monster x[5]={{"dd",5},{"ded",10,20}};
+    p=new monster("fs",10,50);
+    delete p;
+
+
     return 0;
+
 }
+
--- monster.h
+++ monster.h
@@ -10,12 +10,23 @@
 class monster{
 private:
     string name;
-    int hp,potion;
+     int hp,potion;
 public:
     void Attack(monster &);
-          void heal();
-    monster(string, int,int);
+    void heal();
+    monster(string n,int h,int p);
 };
 
+monster::monster(string n ="slime" ,int h=10,int p= 20){ 
+    name =n;
+    h>200?  hp=200:hp=h;
+    p>10?potion=10: potion=p;
 
+    cout<<"Monster "<<name<<" are coming"<<endl;
+}
+/*monster::~monster(){
+    cout<<"bye bye"<<name<<hp:<<hp<<endl;
+}
 #endif /* monster_h */
+
+#endif
